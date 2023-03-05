e2b3b19e6e73a67ed42218c56d713cc8deabcb9a
Shayan Shabihi <shabihish@gmail.com>
2023-03-05 14:42:37 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    let a = req.query.a
-    let b = req.query.b
+   // let a = req.query.a
+   // let b = req.query.b
     console.log("Just got a request!")
-    res.send('' + (a+b))
+
+
+    const axios = require('axios') 
+
+  
+// Make request 
+var a;
+axios.get('https://google.com')
+.then(res => a = res.data) 
+.catch(err => console.log(err))
+ 
+res.send(a)
+
+
 })
 app.listen(process.env.PORT || 3000)
