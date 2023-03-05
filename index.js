6e814684ccc2ac0a59172d0a740c91cb0dee47ee
Shayan Shabihi <shabihish@gmail.com>
2023-03-05 13:59:36 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
+    let a = req.query.a
+    let b = req.query.b
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('' + (a+b))
 })
 app.listen(process.env.PORT || 3000)
